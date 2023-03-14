4f3c560c4c6a3520087fb4770c5f07b7c1bd189a
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2023-03-14 07:58:40 +0000

    Add browser client options

--- src/Supabase/AuthHelpers.purs
+++ src/Supabase/AuthHelpers.purs
@@ -1,6 +1,9 @@
 module Supabase.AuthHelpers
-  ( UseUser
+  ( ClientOptions
+  , CookieOptions
+  , UseUser
   , createBrowserClient
+  , createBrowserClientWithOptions
   , useUser
   , userProvider
   ) where
@@ -11,10 +14,12 @@ import Data.Maybe (Maybe)
 import Data.Nullable (Nullable)
 import Data.Nullable as Nullable
 import Effect (Effect)
+import Foreign (Foreign)
 import React.Basic.Hooks (Hook, ReactComponent, unsafeHook)
 import Record.Studio (mapRecordKind)
 import Supabase.Supabase as Supabase
 import Supabase.Types (Client)
+import Yoga.JSON as YogaJSON
 
 foreign import userProvider :: forall props. ReactComponent { supabaseClient :: Client | props }
 
@@ -26,3 +31,21 @@ useUser = unsafeHook useUserImpl <#> mapRecordKind Nullable.toMaybe
 foreign import data UseUser :: Type -> Type
 
 foreign import createBrowserClient :: Effect Client
+
+type CookieOptions =
+  { domain :: Maybe String
+  , maxAge :: Maybe Int
+  , name :: Maybe Int
+  , path :: Maybe String
+  , sameSite :: Maybe String
+  , secure :: Maybe Boolean
+  }
+
+type ClientOptions =
+  { cookieOptions :: Maybe CookieOptions
+  }
+
+foreign import createBrowserClientWithOptionsInternal :: Foreign -> Effect Client
+
+createBrowserClientWithOptions ∷ ClientOptions → Effect Client
+createBrowserClientWithOptions = YogaJSON.write >>> createBrowserClientWithOptionsInternal
