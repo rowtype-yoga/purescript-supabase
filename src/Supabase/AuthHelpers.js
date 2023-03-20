9ee49072c30502caede8808fb58763e082b6c484
Mark Eibes <mark.eibes@gmail.com>
2023-03-20 19:45:58 +0100

    Remove non-existing UserProvider

--- src/Supabase/AuthHelpers.js
+++ src/Supabase/AuthHelpers.js
@@ -1,6 +1,5 @@
-import { UserProvider, useUser } from "@supabase/auth-helpers-react";
+import { useUser } from "@supabase/auth-helpers-react";
 
-export const userProvider = UserProvider;
 export const useUserImpl = useUser;
 
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
